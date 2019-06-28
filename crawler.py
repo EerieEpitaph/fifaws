84e5b7f38d3934020e9067fdba09bae06a14a4b2
EerieEpitaph <o.lischio@studenti.uniba.it>
2019-06-29 00:21:35 +0200

    Testing detailed info

--- crawler.py
+++ crawler.py
@@ -11,12 +11,12 @@ class Bailout(Exception):
 
 # Base program informations (to be argv-ed)
 datasets_path = "./datasets/"
-maxPlayerOffset = 10  # Default = 350 for 20 000+ players
+maxPlayerOffset = 1  # Default = 350 for 20 000+ players
 
 # Core-data initializing
 root_url = "https://sofifa.com"
 columns = ['ID', 'Name', 'Age', 'Nationality', 'Overall', 'Potential', 'Club', 'Value', 'Wage', 'TotStats']
-data = pd.DataFrame(columns = columns)
+data = pd.DataFrame(columns=columns)
 
 # Make datasets target folder
 if not os.path.exists(datasets_path):
@@ -59,7 +59,7 @@ for opt in optgroups:
 dataset_len = len(dataset_links)
 
 for i in range(dataset_len):
-    print("\nGrabbing " + dataset_dates[i] + " (" + str(i) + " of " + str(dataset_len) + ")")
+    print("\nGrabbing " + dataset_dates[i] + " (" + str(i+1) + " of " + str(dataset_len) + ")")
     try:
         data = pd.DataFrame(columns=columns)
         currOffset = 1
@@ -101,65 +101,38 @@ for i in range(dataset_len):
 
     except Bailout:
         pass
+    ######################################
+    # Detailed info of player, THANKS! to -insert author, can't remember now-
+    detailed_columns = ['Preferred Foot', 'International Reputation', 'Weak Foot', 'Skill Moves', 'Work Rate',
+                        'Body Type', 'Position', 'Height', 'Weight', 'LS', 'ST', 'RS', 'LW', 'LF', 'CF', 'RF', 'RW',
+                        'LAM', 'CAM', 'RAM', 'LM', 'LCM', 'CM', 'RCM', 'RM', 'LWB', 'LDM', 'CDM', 'RDM', 'RWB', 'LB',
+                        'LCB', 'CB', 'RCB', 'RB', 'Crossing', 'Finishing', 'HeadingAccuracy', 'ShortPassing', 'Volleys',
+                        'Dribbling', 'Curve', 'FKAccuracy', 'LongPassing', 'BallControl', 'Acceleration', 'SprintSpeed',
+                        'Agility', 'Reactions', 'Balance', 'ShotPower', 'Jumping', 'Stamina', 'Strength', 'LongShots',
+                        'Aggression', 'Interceptions', 'Positioning', 'Vision', 'Composure', 'Marking',
+                        'StandingTackle', 'SlidingTackle', 'GKDiving', 'GKHandling', 'GKKicking', 'GKPositioning',
+                        'GKReflexes', 'ID']
+    detailed_data = pd.DataFrame(index=range(0, data.count()[0]), columns=detailed_columns)
+    detailed_data.ID = data.ID.values
+    player_data_url = 'https://sofifa.com/player/'
+    for id_final in data.ID:
+        url = player_data_url + str(id_final)
+        print(url)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, 'html.parser')
+        body = soup.find('body').find('div', recursive=False).find('div', recursive=False)
+        player_card = soup.find("div", attrs={"class": "bp3-card player"})
+        aside = soup.find("div", attrs={"class": "bp3-callout spacing calculated"})
+        print(player_card)
+        print('\n\n')
+        print(aside)
+        exit(0)
+
+        skill_map = {}
+
 
     data = data.drop_duplicates()
     sane_date = re.sub(' ', '_', dataset_dates[i])
-    data.to_csv(datasets_path + sane_date + '.csv', encoding='utf-8-sig', index=False)
-
-'''
-# Informazioni dettagliate sul singolo giocatore
-detailed_columns = ['Preferred Foot', 'International Reputation', 'Weak Foot', 'Skill Moves', 'Work Rate', 'Body Type', 'Real Face', 'Position', 'Jersey Number', 'Joined', 'Loaned From', 'Contract Valid Until', 'Height', 'Weight', 'LS', 'ST', 'RS', 'LW', 'LF', 'CF', 'RF', 'RW', 'LAM', 'CAM', 'RAM', 'LM', 'LCM', 'CM', 'RCM', 'RM', 'LWB', 'LDM', 'CDM', 'RDM', 'RWB', 'LB', 'LCB', 'CB', 'RCB', 'RB', 'Crossing', 'Finishing', 'HeadingAccuracy', 'ShortPassing', 'Volleys', 'Dribbling', 'Curve', 'FKAccuracy', 'LongPassing', 'BallControl', 'Acceleration', 'SprintSpeed', 'Agility', 'Reactions', 'Balance', 'ShotPower', 'Jumping', 'Stamina', 'Strength', 'LongShots', 'Aggression', 'Interceptions', 'Positioning', 'Vision', 'Penalties', 'Composure', 'Marking', 'StandingTackle', 'SlidingTackle', 'GKDiving', 'GKHandling', 'GKKicking', 'GKPositioning', 'GKReflexes', 'ID']
-detailed_data = pd.DataFrame(index = range(0, data.count()[0]), columns = detailed_columns)
-detailed_data.ID = data.ID.values
-
-player_data_url = 'https://sofifa.com/player/'
-for id in data.ID:
-    url = player_data_url + str(id)
-    print(url)
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text, 'html.parser')
-    skill_map = {}
-    columns = soup.find('div', {'class': 'teams'}).find('div', {'class': 'columns'}).findAll('div', {'class': 'column col-4'})
-    for column in columns:
-        skills = column.findAll('li')
-        for skill in skills:
-            if(skill.find('label') != None):
-                label = skill.find('label').text
-                value = skill.text.replace(label, '').strip()
-                skill_map[label] = value
-    meta_data = soup.find('div', {'class': 'meta'}).text.split(' ')
-    length = len(meta_data)
-    weight = meta_data[length - 1]
-    height = meta_data[length - 2].split('\'')[0] + '\'' + meta_data[length - 2].split('\'')[1].split('\"')[0]
-    skill_map["Height"] = height
-    skill_map['Weight'] = weight
-    if('Position' in skill_map.keys()):
-        if skill_map['Position'] in ('', 'RES', 'SUB'):
-            skill_map['Position'] = soup.find('article').find('div', {'class': 'meta'}).find('span').text
-        if(skill_map['Position'] != 'GK'):
-            card_rows = soup.find('aside').find('div', {'class': 'card mb-2'}).find('div', {'class': 'card-body'}).findAll('div', {'class': 'columns'})
-            for c_row in card_rows:
-                attributes = c_row.findAll('div', {'class': re.compile('column col-sm-2 text-center')})
-                for attribute in attributes:
-                    if(attribute.find('div')):
-                        name = ''.join(re.findall('[a-zA-Z]', attribute.text))
-                        value = attribute.text.replace(name, '').strip()
-                        skill_map[str(name)] = value
-    sections = soup.find('article').findAll('div', {'class': 'mb-2'})[1:3]
-    print(sections)
-    first = sections[0].findAll('div', {'class': 'column col-4'})
-    second = sections[1].findAll('div', {'class': 'column col-4'})[:-1]
-    sections = first + second
-    for section in sections:
-        items = section.find('ul').findAll('li')
-        for item in items:
-            value = int(re.findall(r'\d+', item.text)[0])
-            name = ''.join(re.findall('[a-zA-Z]*', item.text))
-            skill_map[str(name)] = value
-    for key, value in skill_map.items():
-        detailed_data.loc[detailed_data.ID == id, key] = value
-
-full_data = pd.merge(data, detailed_data, how = 'inner', on = 'ID')
-full_data.to_csv('data.csv', encoding='utf-8-sig')
-'''
+    full_data = pd.merge(data, detailed_data, how='inner', on='ID')
+    full_data.to_csv(datasets_path + sane_date + '.csv', encoding='utf-8-sig')
